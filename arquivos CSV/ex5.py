ad2d65dd569bcbd0c88f13672511c43194ae932e
PEDRO TEIXEIRA DE FREITAS <149191894+deathcoles@users.noreply.github.com>
2026-03-23 14:28:05 -0300

    Update ex5.py

--- arquivos CSV/ex5.py
+++ arquivos CSV/ex5.py
@@ -2,7 +2,7 @@ with open("turma.csv", "w", encoding="utf-8") as arquivo:
     arquivo.write("nome,nota1,nota2\n")
     
     for i in range(3):
-        print(f"\n--- Dados do Aluno {i+1} ---")
+        print(f"\nDados do Aluno {i+1}")
         nome = input("Digite o nome: ")
         nota1 = input("Digite a primeira nota: ")
         nota2 = input("Digite a segunda nota: ")
